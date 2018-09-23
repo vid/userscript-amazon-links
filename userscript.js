900f7d6a8ae2bd4a9d73b4691a88ae1d60873242
David H. Mason <vid_github@zooid.org>
2018-09-23 14:36:14 -0400

    Create userscript.js

---
+++ userscript.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         amazon improvements
+// @namespace    http://nicer.info
+// @version      0.2
+// @description  support visited links for amazon
+// @author       Vid
+// @match        https://www.amazon.ca/*
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
+(function(doc) {
+
+  const style = document.createElement("style");
+  style.setAttribute("media", "screen")
+  style.appendChild(document.createTextNode(""));
+  document.head.appendChild(style);
+  style.sheet.insertRule("a:visited { color: purple !important }", 0);
+  const anchors = document.getElementsByClassName('s-access-detail-page');
+  for (var i = 0; i < anchors.length; i++) {
+      const anchor = anchors[i];
+      if (anchor.href.includes('qid')) {
+          anchor.href = anchor.href.replace(/qid=\d+/, 'qid=0');
+      }
+  }
+
+
+})(document);
